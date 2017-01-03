0c42f9ef0844d8d24d804490b44187da8587f39e
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2017-01-04 00:05:21 +0200

    Review and comments, and code changes.

--- README.md
+++ README.md
@@ -7,10 +7,16 @@ All installed with the LuaRocks package manager. See OpenResty [docs](https://op
 * lua-resty-session - server session and cookie manager
 * lua-resty-template - html server templates
 * lua-requests - http client for making calls to Auth0 endpoints
-* penlight - pretty printing of lua tables (for debug tracing)
+* penlight - pretty printing of lua tables (for debug tracing) 
 
+**penlight should be removed as a hard dependency, if it is available use it, otherwise just use tostring or something**
+**you don't probably need dependency to lua-resty-template, and that should be removed as well**
+**don't use lua-requests!!! it does not utilize OpenResty co-sockets, please use e.g. [lua-resty-http](https://github.com/pintsized/lua-resty-http) instead**
 
 ## usage
+
+** try to build this as a library. Let user configure this how ever he wants without having him to touch some predefined config file**
+
 Sign into [Auth0.com](https://auth0.com/) and get an authentication client. From the client settings page populate the file config/auth0.conf.lua.
 ```
 auth0conf = {
@@ -23,6 +29,9 @@ auth0conf = {
 ```
 
 Add the config/site.conf into nginx, by copy or by symlink. The remaining files are found using the environment variable AUTH0_BASE_DIR which refers to the root of this repository. The following snippet shows how this environment variable is used
+
+**I have commented this in site.conf**
+
 ```
 init_by_lua_block {
    auth0BaseDir = os.getenv("AUTH0_BASE_DIR")
@@ -30,7 +39,11 @@ init_by_lua_block {
    auth0 = require "auth0"
 }
 ```
+
 If your using systemd you can set an environment variable in the service unit file using the Environment property in the Service section
+
+**I'm not agains't environment vars, but that should probably be the last place to store this kind of configs.**
+
 ```
 Environment=AUTH0_BASE_DIR=path-goes-here
 ```
--- lua/auth0.lua
+++ lua/auth0.lua
@@ -1,106 +1,142 @@
+local requests = require "requests"
+-- I'm really not sure about this,
+-- but it is better than depending
+-- on external file. But really this
+-- should be something that should
+-- just go to documentation.
+local template = require "resty.template".compile[[
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>{{auth0Config.APP_NAME}} Login</title>
+</head>
+<body>
+  <h1>{{message}}</h1>
+  <script src="https://cdn.auth0.com/js/lock/10.6/lock.min.js"></script>
+  <script type="text/javascript">
+    var lock = new Auth0Lock('{{auth0Config.AUTH0_CLIENT_ID}}', '{{auth0Config.AUTH0_DOMAIN}}', {
+      auth: {
+        redirectUrl: '{*auth0Config.AUTH0_CALLBACK_URL*}'
+      }
+    });
+
+    function signin() {
+      lock.show();
+    }
+  </script>
+  <button onclick="signin()">Login</button>
+</body>
+</html>
+]]
+local session = require "resty.session"
+local redirect = ngx.redirect
+local format = string.format
+local concat = table.concat
+local assert = assert
+local uargs = ngx.req.get_uri_args
+local gsub = string.gsub
+local byte = string.byte
+local var = ngx.var
+local say = ngx.say
+
 local function urlencode(str)
-   if (str) then
-      str = string.gsub (str, "\n", "\r\n")
-      str = string.gsub (str, "([^%w ])",
-         function (c) return string.format ("%%%02X", string.byte(c)) end)
-      str = string.gsub (str, " ", "+")
+   if str then
+      str = gsub (str, "\n", "\r\n")
+      str = gsub (str, "([^%w ])", function (c) return format("%%%02X", byte(c)) end)
+      str = gsub (str, " ", "+")
    end
    return str
 end
 
 local function getaccesstoken(authorization_code, config)
-    local requests = require("requests")
-    local data = "client_id=" .. config.AUTH0_CLIENT_ID ..
-                  "&client_secret=" .. config.AUTH0_CLIENT_SECRET ..
-                  "&redirect_uri=" .. urlencode(config.AUTH0_CALLBACK_URL) ..
-                  "&code=" .. authorization_code .. "&grant_type=authorization_code"
+    local data = concat{ "client_id=", config.AUTH0_CLIENT_ID,
+                  "&client_secret=", config.AUTH0_CLIENT_SECRET,
+                  "&redirect_uri=", urlencode(config.AUTH0_CALLBACK_URL),
+                  "&code=", authorization_code, "&grant_type=authorization_code" }
     local headers = {}
     headers["Content-Type"] = "application/x-www-form-urlencoded"
-    local response = requests.post{url = "https://".. config.AUTH0_DOMAIN .. "/oauth/token", data = data, headers = headers}
-    if response.status_code ~= 200 then error(response.text) end
-    local json_body, error = response.json()
-    if error then error(error) end
-    return json_body.access_token
+    local response = requests.post{ url = concat{ "https://", config.AUTH0_DOMAIN, "/oauth/token" }, data = data, headers = headers }
+    assert(response.status_code == 200, response.text)
+    local json, error = response.json()
+    assert(json, error)
+    return json.access_token
 end
 
 local function getuserprofile(access_token, config)
-    local requests = require("requests")
-    local response = requests.get("https://".. config.AUTH0_DOMAIN .. "/userinfo?access_token=" .. access_token)
-    if response.status_code ~= 200 then error(response.text) end
-    local json_body, error = response.json()
-    if error then error(error) end
-    return json_body
+    local response = requests.get(concat{ "https://", config.AUTH0_DOMAIN, "/userinfo?access_token=", access_token })
+    assert(response.status_code == 200, response.text)
+    local json, error = response.json()
+    assert(json, error)
+    return json
 end
 
-local function writelog(tbl)
-   local msg = require 'pl.pretty'.write(tbl)
-   ngx.log(ngx.NOTICE, msg)
-   return msg
-end
+-- This should be removed!
+-- It introduces a weak dependency
+-- that should be avoided as it is
+-- not needed anywhere else.
+--local function writelog(tbl)
+--   local msg = require 'pl.pretty'.write(tbl)
+--   log(NOTICE, msg)
+--   return msg
+--end
 
-local function loadconfig(filepath)
-  if not filepath then
-    local auth0BaseDir = os.getenv("AUTH0_BASE_DIR")
-    filepath = auth0BaseDir .. "/config/auth0.conf.lua"
-  end
-  local ok,e = pcall(dofile,filepath)
-  if not ok then
-    error(e)
-  end
-  return auth0conf
-end
+-- This should be removed!
+--
+--local function loadconfig(filepath)
+--  if not filepath then
+--    local auth0BaseDir = os.getenv("AUTH0_BASE_DIR")
+--    filepath = auth0BaseDir .. "/config/auth0.conf.lua"
+--  end
+--  assert(pcall(dofile, filepath))
+--  return auth0conf
+--end
 
 local _M = {
-  config = loadconfig()
+  --config = loadconfig()
 }
 
-function _M.printconfig()
-   writelog("printing config")
-   writelog(_M.config)
-end
+-- This should be removed!
+--function _M.printconfig()
+--   writelog("printing config")
+--   writelog(_M.config)
+--end
 
-function _M.writelog(tbl)
-   writelog(tbl)
-end
+-- This should be removed!
+--function _M.writelog(tbl)
+--   writelog(tbl)
+--end
 
 function _M.isauth()
-  local session = require "resty.session".open()
+  local session = session.open()
   if not session.present or not session.data.user then
     ngx.status = 401 -- unauthorized
     session:start()
-    session.data.original_url = ngx.var.request_uri
+    session.data.original_url = var.request_uri
     session:save()
-    local template = require "resty.template"
-    template.render("login.html", { auth0Config = _M.config })
+    say(template{ auth0Config = _M.config })
   end
 end
 
 function _M.login()
-  local session = require "resty.session".open()
-  if not session.present then
-      session:start()
-      session:save()
-  end
-  local template = require "resty.template"
-  template.render("login.html", { auth0Config = _M.config })
+  session.start()
+  say(template{ auth0Config = _M.config })
 end
 
 function _M.callback()
-  local session = require "resty.session".open()
-  if not session.present then
-      error("no session")
-  end
-  local query_string = ngx.req.get_uri_args()
-  if not query_string["code"] then return error("expected code query parameter") end
-  local token = getaccesstoken(query_string["code"], _M.config)
+  local session = session.open()
+  assert(session.present, "no session")
+  local q = uargs()
+  assert(q.code, "expected code query parameter")
+  local token = getaccesstoken(q.code, _M.config)
   local profile = getuserprofile(token, _M.config)
+  session:start()
   session.data.user = profile
   session:save()
   if session.data.original_url then
-      ngx.redirect(session.data.original_url)
-  else
-      ngx.redirect("/")
+      return redirect(session.data.original_url)
   end
+  return redirect "/"
 end
 
 return _M
